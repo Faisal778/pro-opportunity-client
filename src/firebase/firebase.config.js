75c9b01380d4b07319196ec7c80687bdd202e48c
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-13 19:29:45 +0600

    firebase.config.js setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0tyQgj1HKy64TM6fmkzf0IbidCaUjXHw",
+  authDomain: "proopportunity-6d3a5.firebaseapp.com",
+  projectId: "proopportunity-6d3a5",
+  storageBucket: "proopportunity-6d3a5.appspot.com",
+  messagingSenderId: "896851148801",
+  appId: "1:896851148801:web:e28670c12f9147e0d78000"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
