060399a0c9a8b4ff17b4b0c3b8406f0ac8c643d6
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-13 18:48:02 +0600

    react-router-dom firebase axios react-tabs react-hot-toast swiper date-fns installed, default files cleaned

---
+++ src/layouts/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            {/* Navbar */}
+
+
+            {/* Outlet */}
+
+
+            {/* Footer */}
+        </div>
+    );
+};
+
+export default Root;
