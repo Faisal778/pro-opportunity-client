f97a70c5a6adbcbfbfe3d85990aacc21dfbfa43f
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-13 21:06:32 +0600

    navbar and conditional log in and user profile on right side of navbar done

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Root = () => {
     return (
         <div>
             {/* Navbar */}
-                <h1>main</h1>
+              <Navbar />
 
             {/* Outlet */}
 
