4a629a1f9617e494734b17b01706f307fe1f0f3f
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-15 15:56:35 +0600

    Job details page apply now modal added

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -34,10 +34,53 @@ const JobDetails = () => {
         </div>
 
 
-<div className=' text-center my-5'>
+{/* <div className=' text-center my-5'>
 <button className="btn btn-sm btn-wide">Wide</button>
+</div> */}
+{/* You can open the modal using document.getElementById('ID').showModal() method */}
+<div className='text-center'>
+<button className="btn btn-sm mt-4 btn-wide" onClick={()=>document.getElementById('my_modal_3').showModal()}>Apply Now</button>
 </div>
+<dialog id="my_modal_3" className="modal">
+  <div className="modal-box">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+    <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Account settings</h2>
 
+    <form>
+        <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="username">Username</label>
+                <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="emailAddress">Email Address</label>
+                <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="password">Password</label>
+                <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" htmlFor="passwordConfirmation">Password Confirmation</label>
+                <input id="passwordConfirmation" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+            </div>
+        </div>
+
+        <div className="flex justify-end mt-6">
+            <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+        </div>
+    </form>
+</section>
+
+  </div>
+</dialog>
 
     </div>
 </div>
